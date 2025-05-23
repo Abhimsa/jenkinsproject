07309287fd8199ff33ae3b888cb3e1650022542a
Abhimsa <128863782+Abhimsa@users.noreply.github.com>
2025-05-23 17:18:03 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,44 @@
+public class LinkedList {
+
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.printList(); // Output: 1 2 3
+    }
+}
